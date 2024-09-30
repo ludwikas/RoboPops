d3efda19a2c59384f869e8e404fcc10b175384fd
Andreas <andreasmananas@gmail.com>
2024-09-30 16:09:44 +0200

    sending information to roboto controller file

---
+++ send_urscript_through_python.py
@@ -0,0 +1,26 @@
+import socket
+
+def send_urscript(file_path: str, robot_ip: str, port: int = 30002):
+    
+
+    with open(file_path, "r") as file:
+        urscript = file.read()
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((robot_ip, port))
+
+
+    try:
+        print("Sending URscript to the robot controller")
+        s.sendall(urscript.encode("utf-8"))
+
+        response = s.recv(1024).decode("utf-8")
+        print("Respnse fromm robot:", response)
+    finally:
+        s.close()
+        print("Connection closed.")
+
+file_path = "URscript.txt"
+robot_ip = "192.168.40.128"
+
+send_urscript(file_path, robot_ip)
