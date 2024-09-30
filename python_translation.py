493c985dcdf52b46726d1c7bc2cb5d532a76c743
ludwikas <ludwika.buczynska24@gmail.com>
2024-09-30 21:35:11 +0200

    translation is working

--- python_translation.py
+++ python_translation.py
@@ -1,7 +1,3 @@
 import urx
 robot = urx.Robot('IP')
-# Start of the program
-robot.movej([[1.4265, -0.8756, 1.0797, -1.7749, -1.5708, -0.1443], acc=3.1416, vel=0.3142, radius=DefaultZone])
-robot.movel([p[0, -0.31366, 0.00052, 2.22144, 2.22144, 0], acc=1, vel=Speed000, radius=DefaultZone])
-# End of the program
 robot.close()
