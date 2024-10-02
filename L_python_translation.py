0ea9a1ed6db2a296a88272bc623208b5c8eb7fa1
ludwikas <ludwika.buczynska24@gmail.com>
2024-10-02 15:20:41 +0200

    progress i guess

---
+++ L_python_translation.py
@@ -0,0 +1,9 @@
+import urx
+import socket
+robot_id = '192.168.177.128'
+robot = urx.Robot(robot_id)
+# Start of the program
+robot.movej([[1.4265, -0.599, 1.2775, -2.2493, -1.5708, -0.1443], acc==3.1416, vel==0.3142, radius==DefaultZone])
+robot.movel([p[0, -0.31366, 0.00052, 2.22144, 2.22144, 0], acc==1, vel==DefaultSpeed, radius==DefaultZone])
+# End of the program
+robot.close()
